59a463a9ed642f30a9f81e1cb4d39c13e5a7f274
maclandrol <emmanuel.noutahi@hotmail.ca>
2018-03-20 00:56:00 -0400

    polysolver multsol

--- LabelGTC/lib/PolyRes/PolySolver.py
+++ LabelGTC/lib/PolyRes/PolySolver.py
@@ -23,7 +23,7 @@ class PolytomySolver:
 
     print "COST=", ps.getTableValue(specietree, 1)
 
-    r = ps.getResolutions()
+    r = ps.getResolution()
     print r
     """
 
@@ -40,6 +40,11 @@ class PolytomySolver:
         self.dp_values = {}
         self.special_species_dupcost = {}
         self.special_species_losscost = {}
+        self.reverseMap = {}
+        for g, s in lcaMapping.items():
+            glist = self.reverseMap.get(s.name, [])
+            glist.append(g.name)
+            self.reverseMap[s.name] = glist
 
     def setDupLossCosts(self, dupcost, losscost):
         self.dupcost = dupcost
@@ -298,7 +303,8 @@ class PolytomySolver:
                       k, "v =", v, " vright =", vright)
 
             if v == 0:
-                trees_to_return = [s.name] * k
+                trees_to_return = [(self.reverseMap[s.name].pop() if not s.name.startswith(
+                    '[') else s.name) for i in xrange(k)]  # this here is were to change
             # get k + 1 guys, merge 2
             # TODO : we just take one way of doing this
             elif v == vright + s_dupcost:
@@ -448,7 +454,7 @@ class GeneTreeSolver:
         cpt = 1
         for t in tree.traverse("postorder"):
 
-            if t.name == "" or t.name == "NoName":
+            if not t.name or t.name == "NoName":
                 t.name = "[" + str(cpt) + "]"
                 cpt += 1
 
@@ -572,18 +578,17 @@ class GeneTreeSolver:
                                 pos = sol.find(s.name)
 
                                 rchild = self.solutions_per_gene[gchild]
-
                                 for sol_child in rchild:
 
                                     # sol2 = sol[:pos] + "(" + sol_child + ")" + sol[pos + len(s.name):]
                                     sol2 = sol[:pos] + sol_child + \
                                         sol[pos + len(s.name):]
                                     r2.append(sol2)
-                                    
 
                                     # limit -= 1
 
                         # limit = max(limit, 1)
+
                         r = r2
 
                 if len(r) > limit:
@@ -591,5 +596,5 @@ class GeneTreeSolver:
                 self.solutions_per_gene[g] = r
                 remainingLimit = limit - len(r) + 2
                 # remainingLimit = max(remainingLimit, 1)
-            
+
         return self.solutions_per_gene[self.genetree]
